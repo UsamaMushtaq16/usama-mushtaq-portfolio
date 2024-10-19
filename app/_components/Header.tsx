f530106e105d4f936988da1b3caf5f48790b8cf2
Usama Mushtaq <usamamushtaq.dev@gmail.com>
2024-10-19 14:02:38 +0500

    Header Set

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -14,7 +14,9 @@ export default function Header({ logo }: { logo: string }) {
 
     const [navCollapse, setNavCollapse] = useState(true)
     const [scroll, setScroll] = useState(false)
+    const navs = ['home', 'projects', 'services', 'about', 'experience',]
     const { theme, setTheme } = useTheme()
+    
 
     useEffect(() => {
         const updateScroll = () => {
@@ -24,16 +26,14 @@ export default function Header({ logo }: { logo: string }) {
     }, [])
 
 
-    const navs = ['home', 'projects', 'services', 'about', 'experience',]
 
     return (
         <header className={`backdrop-filter backdrop-blur-lg ${scroll ? 'border-b bg-white bg-opacity-40' : 'border-b-0'} dark:bg-grey-900 dark:bg-opacity-40 border-gray-200 dark:border-b-0 z-30 min-w-full flex flex-col fixed`}>
-            <nav className='lg:w-11/12 2xl:w-4/5 w-full md:px-6 2xl:px-0 mx-auto py-4 hidden sm:flex items-center justify-between'>
-
+            
+            <nav className='lg:w-11/12 2xl:w-4/5 w-full md:px-6 2xl:px-0 mx-auto py-4 sm:flex items-center justify-between max-sm:hidden'> 
                 <Link href={'/'} className='2xl:ml-6 flex items-center gap-2'>
                     <Image className={'nav-logo-image'} src={logoIcon} alt="Logo Gif" /><span className='text-lg font-bold'>{logo}</span>
                 </Link>
-
                 <ul className='flex items-center gap-8'>
                     {navs.map((e, i) => (
                         <li key={i}>
@@ -49,32 +49,20 @@ export default function Header({ logo }: { logo: string }) {
                             </ScrollLink>
                         </li>
                     ))}
-                    {/* <span
-                        onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
-                        className='hover:bg-gray-100 hover:dark:bg-violet-700 p-1.5 rounded-full cursor-pointer transition-colors'>
-                        {theme === 'dark' ? <FiSun /> : <FiMoon />}
-                    </span> */}
                 </ul>
             </nav>
 
-            <nav className='p-4 flex sm:hidden items-center justify-between'>
+            <nav className='p-4 flex sm:hidden items-center justify-between'>    
                 {logo === 'Usama' ? <FaNodeJs size={28} /> : <span className='text-lg font-medium'>{logo.split(' ')[0]}</span>}
                 <div className='flex items-center gap-4'>
-                    {/* <span
-                        onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
-                        className='bg-gray-100 dark:bg-violet-700 p-1.5 rounded-full cursor-pointer transition-colors'>
-                        {theme === 'dark' ? <FiSun /> : <FiMoon />}
-                    </span> */}
                     <CgMenuRight size={20} onClick={() => setNavCollapse(false)} />
                 </div>
             </nav>
 
             <div className={`flex min-h-screen w-screen absolute md:hidden top-0 ${!navCollapse ? 'right-0' : 'right-[-100%]'} bottom-0 z-50 ease-in duration-300`}>
                 <div className="w-1/4" onClick={() => setNavCollapse(true)}></div>
-
                 <div className="flex flex-col p-4 gap-5 bg-gray-100/95 backdrop-filter backdrop-blur-sm dark:bg-grey-900/95 w-3/4">
                     <CgClose className='self-end my-2' size={20} onClick={() => setNavCollapse(true)} />
-
                     {navs.slice(0, 5).map((e) => (
                         <ScrollLink
                             key={e}
